5a7f4231c6ad274fd43099a5cca4593249f9720d
seong-ye <90557778+Seong-ye@users.noreply.github.com>
2021-10-15 17:32:20 +0900

    백준 : 10950 예제
    Scanner 입력받아 A+B

---
+++ backjoon/Main10950.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main10950 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int T = scan.nextInt(); 
+		
+		for(int i=1; i<=T; i++) {
+			int A = scan.nextInt();
+			int B = scan.nextInt();
+			System.out.println(A+B);
+		}
+		
+	}
+
+}
