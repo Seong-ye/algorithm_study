531343058f5c956961400c157ff21b696546c45c
seong-ye <90557778+Seong-ye@users.noreply.github.com>
2021-10-24 00:07:53 +0900

    백준 : 2439 예제
    별 거꾸로 찍는 예제

---
+++ backjoon/Main2439.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main2439 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=n-i; j>=0; j--) {
+				System.out.print(" ");
+			}
+			for(int k=1; k<=i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+	}
+}
