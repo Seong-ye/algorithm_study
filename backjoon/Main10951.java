29c62c60647ecf9cb634c69d87cb0f647392035f
seong-ye <90557778+Seong-ye@users.noreply.github.com>
2021-10-24 00:37:18 +0900

    백준 : 10951 예제
    A+B EOF(End Of File) 이용한 예제

---
+++ backjoon/Main10951.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main10951 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int a=0, b=0, sum=0;
+		
+		while(scan.hasNextInt()) {
+			a = scan.nextInt();
+			b = scan.nextInt();
+			
+			System.out.println(a+b);
+			
+		}
+
+	}
+
+}
