3651e92505b0f9de03f90e6cf2b1d8bb84413881
seong-ye <90557778+Seong-ye@users.noreply.github.com>
2021-10-23 23:37:07 +0900

    백준 : 2438 예제
    입력받은 숫자만큼 별찍기

---
+++ backjoon/Main2438.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main2438 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		if(n>=1 && n<=100) {
+			for (int i=1; i<=n; i++) {
+				for(int j=1; j<=i; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+
+	}
+
+}
